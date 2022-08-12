e30fc43cbff200cf4a1a1619cb5b8a957e66af53
crywink <30543384+crywink@users.noreply.github.com>
2022-08-12 13:15:38 -0500

    Create init.lua

---
+++ src/init.lua
@@ -0,0 +1,15 @@
+--[[
+    Data Handler
+    Author: Samuel#0440 / crywink
+--]]
+
+-- Services
+
+-- Constants
+local Profiles = require(script.profiles)
+local Handler = require(script.handler)
+
+-- Variables
+
+-- Main
+return Handler.new(Profiles)
